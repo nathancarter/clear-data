8d07b9b23024aacfca283c514fdbe9ac8961cb5b
Nathan Carter <ncarter@bentley.edu>
2022-09-16 08:47:39 -0400

    Improve source code docs to work with pdoc3

--- src/clear_data/finite_function.py
+++ src/clear_data/finite_function.py
@@ -3,23 +3,27 @@ import pandas as pd
 
 class FiniteFunction:
     """
-    A FiniteFunction is made up of a finite list of ordered pairs in which each
-    input (left hand element in a pair) is paired with exactly one corresponding
-    output (right hand element in the pair).  We construct FiniteFunction
-    instances by passing the domain (sequence of inputs) and the range
-    (sequence of outputs) to the constructor, and it pairs them up in the
-    obvious way, throwing an error if the above condition is not met.  One can
-    then do lookup into these input-and-output arrays by calling the function,
-    or invert it if possible with inverse().
+    A `FiniteFunction` is made up of a finite list of ordered pairs in which
+    each input (left hand element in a pair) is paired with exactly one
+    corresponding output (right hand element in the pair).  We construct
+    `FiniteFunction` instances by passing the domain (sequence of inputs) and
+    the range (sequence of outputs) to the constructor, and it pairs them up in
+    the obvious way, throwing an error if the above condition is not met.  One
+    can then do lookup into these input-and-output arrays by calling the
+    function, or invert it if possible with `inverse()`.
     """
 
     def __init__ ( self, inputs, outputs ):
         """
-        Create a new FiniteFunction.  Pass a list, array, or Series as each
-        argument.  For example, the "square" function on the first 5 integers
-        can be built by FiniteFunction([1,2,3,4,5],[1,4,9,16,25]).  Throws an
-        error if your inputs don't define a function, as in
-        FiniteFunction([1,2,1],[1,2,3]) (what does 1 map to?).
+        To create a new `FiniteFunction`, pass a list, array, or Series as each
+        argument.  For example, the squaring function on the first 5 integers
+        can be built by `f = FiniteFunction([1,2,3,4,5],[1,4,9,16,25])`.  Throws
+        an error if your inputs don't define a function, as in
+        `FiniteFunction([1,2,1],[1,2,3])`.  (What does 1 map to?)
+
+        You can use instances with ordinary function appication notation, as in
+        the following example.  If you had `f` as defined above, you could
+        evaluate it on the input 2 with the code `f( 2 )` (which returns 4).
         """
         if not isinstance( inputs, pd.Series ):
             inputs = pd.Series( inputs ) # defaults to range index -- good
@@ -35,7 +39,7 @@ class FiniteFunction:
     def __call__ ( self, input ):
         """
         Apply the function by looking the input up in the original set of
-        inputs and returning the corresponding output.  Returns None if the
+        inputs and returning the corresponding output.  Returns `None` if the
         input is not in the function's domain.
         """
         try:
@@ -45,46 +49,46 @@ class FiniteFunction:
     
     def domain ( self ):
         """
-        The set of inputs provided at construction time.
+        The inputs provided at construction time.
         """
         return self._inputs
     
     def inputs ( self ):
         """
-        Same as domain().
+        Same as `domain()`.
         """
         return self._inputs
     
     def range ( self ):
         """
-        The set of outputs provided at construction time.
+        The outputs provided at construction time.
         """
         return self._outputs
     
     def outputs ( self ):
         """
-        Same as range().
+        Same as `range()`.
         """
         return self._outputs
     
     def is_injective ( self ):
         """
-        Same as is_invertible().
+        Same as `is_invertible()`.
         """
         return self._outputs.is_a_function_to( self._inputs )
     
     def is_invertible ( self ):
         """
         Whether this function's outputs are also a function to its inputs,
-        meaning that the function has an inverse.  You can create it with
-        inverse().
+        meaning that the function has an inverse.  If this function returns
+        `True`, you can create the inverse with `inverse()`.
         """
         return self.is_injective()
     
     def inverse ( self ):
         """
-        The finite function mapping this function's outputs to their
+        The `FiniteFunction` mapping this function's outputs to their
         corresponding inputs.  Trying to create this will throw an error unless
-        is_invertible() holds.
+        `is_invertible()` holds.
         """
         return FiniteFunction( self._outputs, self._inputs )
--- src/clear_data/series_extensions.py
+++ src/clear_data/series_extensions.py
@@ -7,8 +7,11 @@ def series_has_duplicates ( self ):
 	Does this Series have any duplicate entries?
 	Returns a boolean result.
 
-	This is equivalent to asking whether any entry in the series' duplicated()
+	This is equivalent to asking whether any entry in the series' `duplicated()`
 	array is true.
+
+	This function is added to the `Series` class, so you can call it as
+	`mySeries.has_duplicates()`.
 	"""
 	return any( self.duplicated() )
 
@@ -18,13 +21,17 @@ def series_has_no_duplicates ( self ):
 	"""
 	Does this Series have all unique entries?
 	Returns a boolean result, which is guaranteed to be the opposie of the
-	result of the function has_duplicates().
+	result of the function `series_has_duplicates()`.
+
+	This function is added to the `Series` class, so you can call it as
+	`mySeries.has_no_duplicates()`.
 	"""
 	return not self.has_duplicates()
 
 pd.Series.has_no_duplicates = series_has_no_duplicates
 
-def is_a_finite_function ( inputs, outputs ):
+# Not a public function; do not make docs for this.
+def _is_a_finite_function ( inputs, outputs ):
 	as_a_dict = dict( zip( inputs, outputs ) )
 	return all( inputs.map( as_a_dict ) == outputs )
 
@@ -36,13 +43,16 @@ def series_is_a_function_to ( self, other_series ):
 	the other_series, so that we could reliably do a lookup operation?
 	The result is a boolean.
 
-	For example, if this series were [1,2,3] and the other ['a','b','a'], the
-	result would be true, because no matter which input we look up (1, 2, or 3)
-	we get a predictable, single output.  But if this series were [1,2,3,1] and
-	the other ['a','b','a','c'], the result would be false, because for the
-	input 1, it is unclear whether the output should be 'a' or 'c'.
+	For example, if this series were `[1,2,3]` and the other `['a','b','a']`,
+	the result would be true, because no matter which input we look up (1, 2, or
+	3) we get a predictable, single output.  But if this series were `[1,2,3,1]`
+	and the other `['a','b','a','c']`, the result would be false, because for
+	the input 1, it is unclear whether the output should be `'a'` or `'c'`.
+
+	This function is added to the `Series` class, so you can call it as
+	`mySeries.is_a_function_to(yourSeries)`.
 	"""
-	return is_a_finite_function( self, other_series )
+	return _is_a_finite_function( self, other_series )
 
 pd.Series.is_a_function_to = series_is_a_function_to
 
@@ -50,20 +60,26 @@ def series_is_a_function ( self ):
 	"""
 	If we view this Series' index as a list of inputs, and its entries as a
 	corresponding list of outputs, do those pairs form a function?  See the
-	documentation for Series.is_a_function_to(other) for more details; this
+	documentation for `series_is_a_function_to(other)` for more details; this
 	function asks about mapping the Series' inputs to its own entries, rather
 	than a separate Series' entries.
+
+	This function is added to the `Series` class, so you can call it as
+	`mySeries.is_a_function()`.
 	"""
-	return is_a_finite_function( self.index.to_series(), self )
+	return _is_a_finite_function( self.index.to_series(), self )
 
 pd.Series.is_a_function = series_is_a_function
 
 def series_to_function ( self ):
 	"""
-	See the documentation for is_a_function() to understand how to view a
-	Series as a function.  If that test returns True, this function will create
-	a FiniteFunction instance that embodies the function in question, making it
-	easy to apply the function to do lookups.
+	See the documentation for `series_is_a_function()` to understand how to view
+	a Series as a function.  If that test returns True, this function will
+	create a `FiniteFunction` instance that embodies the function in question,
+	making it easy to apply the function to do lookups.
+
+	This function is added to the `Series` class, so you can call it as
+	`mySeries.to_function()`.
 	"""
 	return FiniteFunction( self.index.to_series(), self )
 
@@ -71,10 +87,14 @@ pd.Series.to_function = series_to_function
 
 def series_to_function_to ( self, other_series ):
 	"""
-	See the documentation for is_a_function_to(other) to understand how to view
-	one Series as a function to another Series.  If that test returns True, this
-	function will create a FiniteFunction instance that embodies the function in
-	question, making it easy to apply the function to do lookups.
+	See the documentation for `series_is_a_function_to(other)` to understand how
+	to view one Series as a function to another Series.  If that test returns
+	`True`, this function will create a FiniteFunction instance that embodies
+	the function in question, making it easy to apply the function to do
+	lookups.
+
+	This function is added to the `Series` class, so you can call it as
+	`mySeries.to_function_to(yourSeries)`.
 	"""
 	return FiniteFunction( self, other_series )
 
@@ -82,10 +102,13 @@ pd.Series.to_function_to = series_to_function_to
 
 def series_to_dictionary ( self ):
 	"""
-	See the documentation for is_a_function() to understand how to view a
-	Series as a function.  If that test returns True, this function will create
-	a Python dict that embodies the function in question, making it easy to
-	perform lookup operations.
+	See the documentation for `series_is_a_function()` to understand how to view
+	a Series as a function.  If that test returns True, this function will
+	create a Python dict that embodies the function in question, making it easy
+	to perform lookup operations.
+
+	This function is added to the `Series` class, so you can call it as
+	`mySeries.to_dictionary()` or just as `mySeries.to_dict()`.
 	"""
 	return dict( zip( self.index.to_series(), self ) )
 
@@ -94,10 +117,14 @@ pd.Series.to_dictionary = series_to_dictionary
 
 def series_to_dictionary_to ( self, other_series ):
 	"""
-	See the documentation for is_a_function_to(other) to understand how to view
-	one Series as a function to another Series.  If that test returns True, this
-	function will create a Python dict that embodies the function in question,
-	making it easy to perform lookup operations.
+	See the documentation for `series_is_a_function_to(other)` to understand how
+	to view one Series as a function to another Series.  If that test returns
+	`True`, this function will create a Python dict that embodies the function
+	in question, making it easy to perform lookup operations.
+
+	This function is added to the `Series` class, so you can call it as
+	`mySeries.to_dictionary_to(yourSeries)` or just as
+	`mySeries.to_dict_to(yourSeries)`.
 	"""
 	return dict( zip( self, other_series ) )
 
