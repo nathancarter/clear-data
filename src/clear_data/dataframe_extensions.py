bbd1a15c5a6143e2671d02854369294430de28ec
Nathan Carter <ncarter@bentley.edu>
2022-09-20 16:45:50 -0400

    Extend df.is_a_function to more parameter types

--- src/clear_data/dataframe_extensions.py
+++ src/clear_data/dataframe_extensions.py
@@ -7,8 +7,62 @@ def dataframe_is_a_function ( self, input, output ):
     Does the given DataFrame represent a function from the given input column
     to the given output column?  The answer is computed by treating the two
     columns in question as Series, and simply calling
-    `series_is_a_function_to(other)`; see its documentation for details.
+    `series_is_a_function(other)`; see its documentation for details.
+
+    For the `input` and `output` parameters, you can pass column names, which
+    will be used to choose the relevant columns.  Or you can pass the columns
+    themselves, or any other series of the same length.  Or you can pass the
+    DataFrame's index, which will be converted to a Series for you.
+
+	This function is added to the `DataFrame` class, so you can call it as
+	`df.is_a_function(input_col,output_col)` or as
+    `df.can_lookup(input_col,output_col)`.
     """
-    return self[input].is_a_function( self[output] )
+    # ensure input is a Series
+    if type( input ) is str:
+        input = self[input]
+    elif isinstance( input, pd.Index ):
+        input = input.to_series()
+    # ensure output is a Series
+    if type( output ) is str:
+        output = self[output]
+    elif isinstance( output, pd.Index ):
+        output = output.to_series()
+    # do the work
+    return input.is_a_function( output )
 
 pd.DataFrame.is_a_function = dataframe_is_a_function
+pd.DataFrame.can_lookup = dataframe_is_a_function
+
+def dataframe_get_function ( self, input, output ):
+    """
+    If the given DataFrame represents a function from the given input column
+    to the given output column, which you can test using
+    `dataframe_is_a_function`, then this function will construct for you a
+    Python function embodying that relationship, that you can subsequently
+    call.
+
+    For the `input` and `output` parameters, you can pass column names, which
+    will be used to choose the relevant columns.  Or you can pass the columns
+    themselves, or any other series of the same length.  Or you can pass the
+    DataFrame's index, which will be converted to a Series for you.
+
+	This function is added to the `DataFrame` class, so you can call it as
+	`df.is_a_function(input_col,output_col)` or as
+    `df.can_lookup(input_col,output_col)`.
+    """
+    # ensure input is a Series
+    if type( input ) is str:
+        input = self[input]
+    elif isinstance( input, pd.Index ):
+        input = input.to_series()
+    # ensure output is a Series
+    if type( output ) is str:
+        output = self[output]
+    elif isinstance( output, pd.Index ):
+        output = output.to_series()
+    # do the work
+    return input.is_a_function( output )
+
+pd.DataFrame.is_a_function = dataframe_is_a_function
+pd.DataFrame.can_lookup = dataframe_is_a_function
--- tests/test_dataframe_basics.py
+++ tests/test_dataframe_basics.py
@@ -9,28 +9,45 @@ import pandas as pd
 
 class TestDataFrameBasics( unittest.TestCase ):
 
-	def test_is_a_function ( self ):
-		df = pd.DataFrame( {
-			"unique_inputs"   : [ 1, 2, 3, 4 ],
-			"repeat_inputs"   : [ 1, 2, 1, 3 ],
-			"unique_outputs"  : [ 'a', 'b', 'c', 'd' ],
-			"repeat_outputs1" : [ 'x', 'x', 'y', 'y' ],
-			"repeat_outputs2" : [ 'x', 'y', 'x', 'y' ]
-		} )
-		# with unique inputs it's always a function
-		self.assertTrue(
-			df.is_a_function( 'unique_inputs', 'unique_outputs' ) )
-		self.assertTrue(
-			df.is_a_function( 'unique_inputs', 'repeat_outputs1' ) )
-		self.assertTrue(
-			df.is_a_function( 'unique_inputs', 'repeat_outputs2' ) )
-		# with repeat inputs it may or may not be, depending on the outputs
-		self.assertFalse(
-			df.is_a_function( 'repeat_inputs', 'unique_outputs' ) )
-		self.assertFalse(
-			df.is_a_function( 'repeat_inputs', 'repeat_outputs1' ) )
-		self.assertTrue(
-			df.is_a_function( 'repeat_inputs', 'repeat_outputs2' ) )
+    def test_is_a_function ( self ):
+        df = pd.DataFrame( {
+            "unique_inputs"   : [ 1, 2, 3, 4 ],
+            "repeat_inputs"   : [ 1, 2, 1, 3 ],
+            "unique_outputs"  : [ 'a', 'b', 'c', 'd' ],
+            "repeat_outputs1" : [ 'x', 'x', 'y', 'y' ],
+            "repeat_outputs2" : [ 'x', 'y', 'x', 'y' ]
+        } )
+        # with unique inputs it's always a function
+        self.assertTrue(
+            df.is_a_function( 'unique_inputs', 'unique_outputs' ) )
+        self.assertTrue(
+            df.is_a_function( 'unique_inputs', 'repeat_outputs1' ) )
+        self.assertTrue(
+            df.is_a_function( 'unique_inputs', 'repeat_outputs2' ) )
+        # with repeat inputs it may or may not be, depending on the outputs
+        self.assertFalse(
+            df.is_a_function( 'repeat_inputs', 'unique_outputs' ) )
+        self.assertFalse(
+            df.is_a_function( 'repeat_inputs', 'repeat_outputs1' ) )
+        self.assertTrue(
+            df.is_a_function( 'repeat_inputs', 'repeat_outputs2' ) )
+        # do same 6 tests but using columns in place of their names
+        # ...sometimes
+        self.assertTrue(
+        	df.is_a_function( df.unique_inputs, 'unique_outputs' ) )
+        self.assertTrue(
+            df.is_a_function( df.unique_inputs, df.repeat_outputs1 ) )
+        self.assertTrue(
+            df.is_a_function( 'unique_inputs', df.repeat_outputs2 ) )
+        self.assertFalse(
+            df.is_a_function( df.repeat_inputs, 'unique_outputs' ) )
+        self.assertFalse(
+            df.is_a_function( df.repeat_inputs, df.repeat_outputs1 ) )
+        self.assertTrue(
+            df.is_a_function( 'repeat_inputs', df.repeat_outputs2 ) )
+        # the index can also be used as a column
+        self.assertTrue( df.is_a_function( df.index, 'unique_outputs' ) )
+        self.assertFalse( df.is_a_function( df.repeat_inputs, df.index ) )
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
